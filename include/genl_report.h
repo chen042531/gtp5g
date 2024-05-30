7dcc303dccde38f0499ea36fcd8d4c689cae1950
ycchen <chen042531.cs03@nctu.edu.tw>
2024-05-30 19:07:01 +0800

    get total uplink/downlink traffic

--- include/genl_report.h
+++ include/genl_report.h
@@ -40,8 +40,16 @@ enum gtp5g_multi_usage_report_attrs {
     __GTP5G_URS_ATTR_MAX,
 };
 
+enum gtp5g_ul_dl_report_attrs {
+    GTP5G_UL = 1,
+    GTP5G_DL,
+
+    __GTP5G_ULDL_ATTR_MAX,
+};
+
 extern int gtp5g_genl_get_usage_report(struct sk_buff *, struct genl_info *);
 extern int gtp5g_genl_get_multi_usage_reports(struct sk_buff *, struct genl_info *);
+extern int gtp5g_genl_get_ul_dl_report(struct sk_buff *, struct genl_info *);
 
 extern int gtp5g_genl_fill_ur(struct sk_buff *, struct usage_report *);
 extern int gtp5g_genl_fill_usage_report(struct sk_buff *, u32, u32, u32, struct usage_report *);
